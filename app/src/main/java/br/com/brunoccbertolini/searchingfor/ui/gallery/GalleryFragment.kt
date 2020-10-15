3e7c3dcd246bda119cada94b70726b4741c0c75f
Bruno Bertolini <bruno.bertolini@hotmail.com>
2020-10-15 17:57:30 -0300

    Initial commit

---
+++ app/src/main/java/br/com/brunoccbertolini/searchingfor/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package br.com.brunoccbertolini.searchingfor.ui.gallery
+
+
+import androidx.fragment.app.Fragment
+import br.com.brunoccbertolini.searchingfor.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
